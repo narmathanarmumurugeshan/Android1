9dc7522f5df632096b58a6eb98a6c0406599261e
narmathanarmumurugeshan <41784287+narmathanarmumurugeshan@users.noreply.github.com>
2018-09-25 16:59:28 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.student.fristpage;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final RatingBar rating = (RatingBar) findViewById(R.id.ratingBar);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String rate = String.valueOf(rating.getRating());
+                Toast.makeText(getApplicationContext(), rate, Toast.LENGTH_SHORT).show();
+
+            }
+
+
+        });
+
+
+    }
+}
